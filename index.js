443acef35b8fadecccfbc3b6c0387b9c56c30c08
AndrewDAntonio <github email address>
2020-03-29 21:08:05 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName(name) {
+   return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(customer) {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(customer) {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = `Scott`
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = `something else`;
+}
